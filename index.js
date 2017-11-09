774da540b2ff38cf49ce6c07e33cb5598d17036f
Tim Sander <tomzander88@yahoo.com>
2017-11-09 14:55:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 function add (a, b) {
   return a + b
 }
+add(5, 2)
 function substract (a, b) {
   return a - b
 }
+substract(2, 4)
 function multiply (a, b) {
   return a * b
 }
+multiply(2, 4)
 function divide (a, b) {
   return a / b
 }
+divide(2, 5)
 function inc (a) {
   return a++
 }
+inc(5)
 function dec (a) {
   return a--
 }
+dec(3)
 function makeInt (a) {
   return parseInt(a)
   return parseInt(a, 10)
 }
+makeInt('10')
+makeInt('wjafpjwf')
+
 function preserveDecimal (a) {
   return parseFloat(a)
 }
+preserveDecimal(2.222)
+preserveDecimal('sldkjflksjf')
+
